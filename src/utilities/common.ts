3e88d89848ff80f07b0a2e4886d58c0dfa8b4934
ThayalanGR <grthayalan18@gmail.com>
2022-04-07 23:33:14 +0530

    iconify packages added

---
+++ src/utilities/common.ts
@@ -0,0 +1,12 @@
+/**
+ *
+ * Simulate Network call delay while development
+ * @param delay in seconds
+ * @default 4000
+ */
+export const introduceDelay = async (delay = 4000): Promise<boolean> =>
+    new Promise((resolve) =>
+        setTimeout(() => {
+            resolve(true);
+        }, delay),
+    );
