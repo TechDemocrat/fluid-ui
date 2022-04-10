589d463975a37ab8a0eb34cd4b06315ea856b42a
manikandanvengatesan <manikandan.vengatesan@gmail.com>
2022-04-10 21:40:37 +0530

    horizontal rule is added

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export { IconButton } from './components/IconButton/IconButton';
 export { Link } from './components/Link/Link';
 export { ContentTypeFlag } from './components/ContentTypeFlag/ContentTypeFlag';
 export { ContentStatusFlag } from './components/ContentStatusFlag/ContentStatusFlag';
+export { HorizontalRule } from './components/HorizontalRule/HorizontalRule';
