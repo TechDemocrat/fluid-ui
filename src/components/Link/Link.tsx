1aa6a6ffeedf88d0468b0b9cfd1eeb79ed77019e
ThayalanGR <grthayalan18@gmail.com>
2022-07-17 23:28:44 +0530

    feat(link): improvements made on link component

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -4,14 +4,18 @@ import styles from './Link.module.scss';
 import cn from 'classnames';
 
 export const Link = (props: ILinkProps) => {
-    const { href, label, title, children, disabled } = props;
+    const { href, label, title, children, disabled, onClick } = props;
     const classnames = cn(styles.linkWrapper, {
         [styles.disabled]: disabled,
     });
 
-    return (
+    return href ? (
         <a className={classnames} title={title} href={href}>
             {children ?? label}
         </a>
+    ) : (
+        <div className={classnames} role="button" onClick={onClick}>
+            {children ?? label}
+        </div>
     );
 };
--- src/components/Link/Link.types.ts
+++ src/components/Link/Link.types.ts
@@ -1,7 +1,12 @@
 export interface ILinkProps {
-    href: string;
+    /**
+     * if passed anchor tag will be rendered as a link
+     * if not passed, will be rendered as a div with role "button"
+     */
+    href?: string;
     label?: string;
     disabled?: boolean;
     title?: string;
     children?: React.ReactNode;
+    onClick?: (event: React.MouseEvent<HTMLAnchorElement | HTMLDivElement, MouseEvent>) => void;
 }
