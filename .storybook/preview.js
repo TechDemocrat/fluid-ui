879f85c117943699811c11290df050936c6a752d
ThayalanGR <grthayalan18@gmail.com>
2022-07-24 11:51:19 +0530

    build(storybook): migrated webpack builder from v4 to v5

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import { ThemeProvider } from '../src/components/ThemeProvider/ThemeProvider';
 import '../src/styles/core.scss';
+import { ThemeProvider } from '../src/components/ThemeProvider/ThemeProvider';
 
 export const parameters = {
     actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
+    },
 };
 
 export const decorators = [(Story) => <ThemeProvider>{Story()}</ThemeProvider>];
